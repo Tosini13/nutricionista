2a0b7669da548172c38f36180aa0a6790bf3362c
Tosini13 <jbartos13@gmail.com>
2022-08-20 15:37:18 +0200

    added hamburger and menu for mobile view

---
+++ app/components/header/Hamburger.tsx
@@ -0,0 +1,35 @@
+import { twMerge } from "tailwind-merge";
+
+type THamburgerProps = {
+  onClick: () => void;
+  open?: boolean;
+};
+
+const Hamburger: React.FC<THamburgerProps> = ({ onClick, open = false }) => {
+  const topStyle = twMerge(
+    "w-full border-b-2 border-gray-600 transition duration-300 ease-out",
+    open ? "translate-y-1.5  rotate-45" : ""
+  );
+
+  const centerStyle = twMerge(
+    "w-full max-w-full border-b-2 border-gray-600 transition-all duration-300 ease-out",
+    open ? "max-w-0" : ""
+  );
+
+  const bottomStyle = twMerge(
+    "w-full border-b-2 border-gray-600 transition duration-300 ease-out",
+    open ? "-translate-y-1.5  -rotate-45" : ""
+  );
+
+  return (
+    <>
+      <div onClick={onClick} className="w-[20px] space-y-1">
+        <div className={topStyle} />
+        <div className={centerStyle} />
+        <div className={bottomStyle} />
+      </div>
+    </>
+  );
+};
+
+export default Hamburger;
--- app/components/header/Header.tsx
+++ app/components/header/Header.tsx
@@ -1,43 +1,95 @@
 import React from "react";
 import logo from "../../../public/logo.svg";
 import Button from "../form/Button";
+import Hamburger from "./Hamburger";
 
 type THeaderProps = {};
 
 const Header: React.FC<THeaderProps> = () => {
+  const [openMenu, setOpenMenu] = React.useState(false);
   return (
-    <header className="px-24 py-4 shadow-md">
-      <div id="headerContent">
-        <div className="space-between flex items-center justify-between">
-          <img src={logo} alt={"logo"} />
-          <ul className="flex pl-3">
-            <MenuItem href="#home" title="home" />
-            <MenuItem href="#sobreMi" title="sobre mi" />
-            <MenuItem href="#servicios" title="servicios" />
-            <MenuItem href="#packs" title="packs" />
-            <MenuItem href="#faq" title="FAQ" />
-            <MenuItem href="#contacto" title="contacto" />
-          </ul>
-          <Button disabled>pide cita</Button>
+    <>
+      <header className="py-4 px-8 shadow-md lg:px-24">
+        <div id="headerContent">
+          <div className="space-between flex items-center justify-between">
+            <img src={logo} alt={"logo"} />
+            <ul className="flex hidden pl-3 lg:relative">
+              <MenuItems />
+            </ul>
+            <Button className="hidden lg:relative" disabled>
+              pide cita
+            </Button>
+            <div className="relative" style={{ zIndex: "102" }}>
+              <Hamburger
+                onClick={() => setOpenMenu((open) => !open)}
+                open={openMenu}
+              />
+            </div>
+            <MenuScreen open={openMenu} hideMenu={() => setOpenMenu(false)} />
+          </div>
         </div>
-      </div>
-    </header>
+      </header>
+    </>
+  );
+};
+
+export default Header;
+
+type TMenuItemsProps = {
+  hideMenu?: () => void;
+};
+
+const MenuItems = ({ hideMenu }: TMenuItemsProps) => {
+  return (
+    <>
+      <MenuItem href="#home" title="home" onClick={hideMenu} />
+      <MenuItem href="#sobreMi" title="sobre mi" onClick={hideMenu} />
+      <MenuItem href="#servicios" title="servicios" onClick={hideMenu} />
+      <MenuItem href="#packs" title="packs" onClick={hideMenu} />
+      <MenuItem href="#faq" title="FAQ" onClick={hideMenu} />
+      <MenuItem href="#contacto" title="contacto" onClick={hideMenu} />
+    </>
   );
 };
 
 type TMenuItemProps = {
   title: string;
   href: string;
+  onClick?: () => void;
 };
 
-const MenuItem: React.FC<TMenuItemProps> = ({ title, href }) => {
+const MenuItem: React.FC<TMenuItemProps> = ({ title, href, onClick }) => {
   return (
     <li className="px-9">
-      <a className="link" href={href} style={{ fontWeight: 400 }}>
+      <a
+        className="link"
+        href={href}
+        style={{ fontWeight: 400 }}
+        onClick={onClick}
+      >
         {title}
       </a>
     </li>
   );
 };
 
-export default Header;
+type TMenuScreenProps = {
+  open: boolean;
+  hideMenu?: () => void;
+};
+
+const MenuScreen: React.FC<TMenuScreenProps> = ({ open, hideMenu }) => (
+  <div
+    className="fixed top-0 left-0 h-screen w-screen bg-white"
+    style={{
+      zIndex: "101",
+      display: open ? "block" : "none",
+    }}
+  >
+    <div className="p-6">
+      <ul className="space-y-2">
+        <MenuItems hideMenu={hideMenu} />
+      </ul>
+    </div>
+  </div>
+);
