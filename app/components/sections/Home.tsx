5604d2f3cf5612f208bc9ec6009bdce7be4c8e7e
Tosini13 <jbartos13@gmail.com>
2022-08-20 14:09:25 +0200

    fixed home section

--- app/components/sections/Home.tsx
+++ app/components/sections/Home.tsx
@@ -1,5 +1,4 @@
 import Button from "../form/Button";
-import ImgFruits from "../../../public/images/fruits.png";
 import ImgWatermelon from "../../../public/images/woman_watermelon_bg_white.png";
 
 const style = {
@@ -13,7 +12,7 @@ const style = {
   } as React.ImgHTMLAttributes<HTMLImageElement>["style"],
   rightImage: {
     position: "absolute",
-    right: "6em",
+    right: "0em",
   } as React.ImgHTMLAttributes<HTMLImageElement>["style"],
 };
 
@@ -22,25 +21,31 @@ type THomeProps = {};
 const Home: React.FC<THomeProps> = () => {
   return (
     <section id="#home" className="relative mb-20 py-10 lg:pt-24">
-      <div className="mx-20 mb-20 flex flex-col items-center pb-20 text-center">
-        <h1 className="title pb-8 pt-6 text-6xl font-bold">
-          ¿Quieres empezar
-          <br /> a cuidar de tu <span className="mainColor">salud</span>?
-        </h1>
-        <p className="pb-10 pb-8 text-xl">
-          Puedo ayudarte a mejorar tu salud y a sentirte mejor
-          <br /> gracias al cambio de hábitos en tu alimentación
-        </p>
-        <Button className={"fontSemiBold"} style={style.button}>
-          Pide Cita
-        </Button>
-        {/* <img src={ImgFruits} alt="fruits" style={style.leftImage} /> */}
-        <img
-          className="pt-20"
-          src={ImgWatermelon}
-          alt="watermelon"
-          style={style.rightImage}
-        />
+      <div className="mx-20 mb-20">
+        <div className="flex flex-col items-center pb-20 text-center">
+          <h1 className="title pb-8 pt-6 text-6xl font-bold">
+            ¿Quieres empezar
+            <br /> a cuidar de tu <span className="mainColor">salud</span>?
+          </h1>
+          <div className="relative w-full">
+            <img
+              className="absolute right-0 top-0"
+              src={ImgWatermelon}
+              alt="watermelon"
+            />
+          </div>
+          <p className="pb-10 pb-8 text-xl">
+            Puedo ayudarte a mejorar tu salud y a sentirte mejor
+            <br /> gracias al cambio de hábitos en tu alimentación
+          </p>
+          <Button className={"fontSemiBold"} style={style.button}>
+            Pide Cita
+          </Button>
+          {/**
+           * @todo implement when img is ready
+           */
+          /* <img src={ImgFruits} alt="fruits" style={style.leftImage} /> */}
+        </div>
       </div>
     </section>
   );
