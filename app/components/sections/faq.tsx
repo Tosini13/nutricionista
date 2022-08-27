75fe649ea348eea32b267b9b9dbb5d1d23988f97
Tosini13 <jbartos13@gmail.com>
2022-08-27 21:54:31 +0200

    added responsive faq

--- app/components/sections/faq.tsx
+++ app/components/sections/faq.tsx
@@ -55,9 +55,9 @@ type TFaqsProps = {};
 
 const Faqs: React.FC<TFaqsProps> = () => {
   return (
-    <section id="#servicios" className="mx-20 min-h-[75vh]">
+    <section id="#servicios" className="min-h-[75vh] md:mx-20">
       <SectionTitle className="mb-10 text-center">FAQ</SectionTitle>
-      <div className="mx-20">
+      <div className="lg:mx-20">
         {faqs.map((faq) => (
           <Faq key={faq.id} faq={faq} />
         ))}
