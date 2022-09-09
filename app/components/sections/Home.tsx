d1fb134d8be0549fa0b7399c41927fb107d3aa22
Tosini13 <jbartos13@gmail.com>
2022-09-09 18:06:16 +0200

    added woman watermelon img

--- app/components/sections/Home.tsx
+++ app/components/sections/Home.tsx
@@ -1,5 +1,5 @@
 import Button from "../form/Button";
-import ImgWatermelon from "../../../public/images/woman_watermelon_bg_white.png";
+import ImgWatermelon from "../../../public/images/woman_watermelon.png";
 import Section from "./Section";
 
 const style = {
@@ -21,18 +21,29 @@ type THomeProps = {};
 
 const Home: React.FC<THomeProps> = () => {
   return (
-    <Section id="home" className="py-10 lg:pt-24">
-      <div className="flex flex-col items-center pb-20 md:text-center">
-        <h1 className="title pb-8 pt-6 text-3xl font-bold sm:text-6xl ">
+    <Section id="home" className="relative py-10 lg:mx-0 lg:pt-24 xl:mx-20">
+      <div
+        className="relative z-[2] pb-20"
+        style={{
+          textShadow: "0px 0px 3px rgba(255,255,255,0.7)",
+        }}
+      >
+        <h1 className="title pb-8 pt-6 text-left text-3xl font-bold sm:text-6xl md:text-center">
           ¿Quieres empezar
           <br /> a cuidar de tu <span className="mainColor">salud</span>?
         </h1>
-        <p className="pb-10 pb-8 text-xl">
+        <div className="insetShadow relative z-[1] float-right block max-w-[50%] translate-x-1/3 lg:hidden">
+          <img src={ImgWatermelon} alt="watermelon" />
+        </div>
+        <p className="pb-10 pb-8 text-left text-xl lg:text-center">
           Puedo ayudarte a mejorar tu salud y a sentirte mejor
           <br className="hidden md:inline" /> gracias al cambio de hábitos en tu
           alimentación
         </p>
-        <Button className={"fontSemiBold w-full sm:w-fit"} style={style.button}>
+        <Button
+          className={"fontSemiBold mx-auto w-full sm:w-fit"}
+          style={style.button}
+        >
           Pide Cita
         </Button>
         {/**
@@ -40,11 +51,9 @@ const Home: React.FC<THomeProps> = () => {
          */
         /* <img src={ImgFruits} alt="fruits" style={style.leftImage} /> */}
       </div>
-      {/* <img
-        className="absolute right-0 bottom-0"
-        src={ImgWatermelon}
-        alt="watermelon"
-      /> */}
+      <div className="insetShadow absolute right-0 bottom-0 z-[1] hidden lg:block">
+        <img src={ImgWatermelon} alt="watermelon" />
+      </div>
     </Section>
   );
 };
