3b70f2540925744fae12743204b4e44633f4d88f
Tosini13 <jbartos13@gmail.com>
2022-10-22 12:10:52 +0200

    fixed shadow

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,12 @@
 module.exports = {
   content: ["./app/**/*.{ts,tsx,jsx,js}"],
   theme: {
-    extend: {},
+    extend: {
+      boxShadow: {
+        'md': '0 2px 6px -1px rgb(0 0 0 / 0.1), 0 2px 4px -2px rgb(0 0 0 / 0.1)',
+        'xl': '0 0px 25px -5px rgb(0 0 0 / 0.1), 0 8px 10px -6px rgb(0 0 0 / 0.1)',
+      }
+    },
   },
   plugins: [],
 };
