10fb043450c077a62caf3f9f1ffc9649d7f558dc
Tosini13 <jbartos13@gmail.com>
2022-12-05 12:33:18 +0100

    fixed autofill input

--- app/components/form/Input.tsx
+++ app/components/form/Input.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import { twMerge } from "tailwind-merge";
 
 type InputPropsType = React.InputHTMLAttributes<HTMLInputElement> & {
-  containerClassName: string;
+  containerClassName?: string;
   error?: string | null;
 };
 
@@ -32,9 +32,10 @@ const Input: React.FC<InputPropsType> = ({
       <input {...props} id={id} className={mergedClassName} placeholder=" " />
       <label
         htmlFor={id}
-        className={
-          "absolute left-0 top-0 -translate-y-[100%] pl-0 text-sm text-gray transition-all duration-200 ease-out peer-placeholder-shown:top-[50%] peer-placeholder-shown:-translate-y-[50%] peer-placeholder-shown:pl-[1.1rem]  peer-placeholder-shown:text-lg peer-focus:top-0 peer-focus:-translate-y-[100%] peer-focus:pl-0 peer-focus:text-sm peer-focus:text-main-light"
-        }
+        className={`absolute left-0 top-0 -translate-y-[100%] pl-0 text-sm text-gray transition-all duration-200 
+          ease-out peer-placeholder-shown:top-[50%] peer-placeholder-shown:-translate-y-[50%]  peer-placeholder-shown:pl-[1.1rem] peer-placeholder-shown:text-lg 
+          peer-autofill:top-0 peer-focus:top-0 peer-focus:-translate-y-[100%] peer-focus:pl-0
+          peer-focus:text-sm peer-focus:text-main-light`}
       >
         {placeholder}
       </label>
