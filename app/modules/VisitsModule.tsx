2f6ebaa517a5f0ee9d58554a14cc2e2fa0b1c3d6
Tosini13 <jbartos13@gmail.com>
2023-01-20 20:49:46 +0100

    fixed visits module for mobile

--- app/modules/VisitsModule.tsx
+++ app/modules/VisitsModule.tsx
@@ -36,20 +36,20 @@ const VisitsModule: React.FC<VisitsModulePropsType> = ({}) => {
   return (
     <Section
       data-testid="visits_module"
-      className={`max-w-none bg-gradient-to-b from-[#FFF] to-primary-light`}
+      className={`mx-0 max-w-none bg-gradient-to-b from-[#FFF] to-primary-light`}
     >
-      <div className="mx-auto max-w-screen-lg">
+      <div className="mx-2 max-w-screen-lg lg:mx-auto">
         <SectionTitle className="mb-20 text-center">
           <span className="text-secondary">¿Como funcionan</span> las visitas
           Online?
         </SectionTitle>
-        <ol className="grid list-[decimal-leading-zero] grid-cols-2 gap-y-10 gap-x-32 pt-20">
+        <ol className="grid list-inside list-[decimal-leading-zero] grid-cols-1 gap-y-10 pt-20 md:grid-cols-2 md:gap-x-10 xl:list-outside xl:gap-x-32">
           {visitSteps.map((step) => (
             <li
-              className="marker:text-4xl marker:font-bold marker:text-primary odd:-mt-20"
+              className="marker:text-4xl marker:font-bold marker:text-primary md:odd:-mt-20"
               key={step.id}
             >
-              <Paragraph className="ml-8">{step.description}</Paragraph>
+              <Paragraph className="md:ml-8">{step.description}</Paragraph>
             </li>
           ))}
         </ol>
