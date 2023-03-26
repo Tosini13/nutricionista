1623447b1d005da71e514be8da293c3055f557fc
Tosini13 <jbartos13@gmail.com>
2023-03-26 10:46:56 +0200

    moved contact form to separate file

--- app/modules/ContactModule.tsx
+++ app/modules/ContactModule.tsx
@@ -1,16 +1,10 @@
 import React from "react";
-import { Form, useActionData, useLoaderData } from "@remix-run/react";
+import { useActionData, useLoaderData } from "@remix-run/react";
 import Paragraph from "~/components/sections/Paragraph";
 import Section from "~/components/sections/Section";
 import SectionTitle from "~/components/sections/SectionTitle";
 import { ActionData, LoaderData } from "~/routes";
-import Textarea from "~/components/form/Textarea";
-import Input from "~/components/form/Input";
-import ReCaptcha from "~/components/form/ReCaptcha";
-import sendIcon from "../../public/img/icons/send.icon.svg";
-import { Button } from "~/components/form/Button";
-
-const inputClassName = "mb-4 sm:mb-0";
+import ContactForm from "~/components/ContactForm";
 
 type ContactModulePropsType = {};
 
@@ -41,64 +35,7 @@ const ContactModule: React.FC<ContactModulePropsType> = ({}) => {
           Si tienes alguna duda escríbeme y me pondré en contacto contigo lo
           antes posible
         </Paragraph>
-        <Form
-          ref={ref}
-          method="post"
-          data-testid="contact_form"
-          className="space-y-8"
-        >
-          <div className="flex flex-col space-y-8 md:flex-row  md:space-y-0 md:space-x-16">
-            <div className="min-w-[300px] space-y-8 lg:min-w-[40%]">
-              <Input
-                id="name"
-                data-testid="contact-input-name"
-                placeholder="Nombre"
-                name="name"
-                containerClassName={inputClassName}
-                error={result?.errors?.name && "Este campo es obligatorio"}
-              />
-              <Input
-                id="surname"
-                data-testid="contact-input-surname"
-                placeholder="Apellido"
-                name="surname"
-                containerClassName={inputClassName}
-                error={result?.errors?.surname && "Este campo es obligatorio"}
-              />
-              <Input
-                id="email"
-                data-testid="contact-input-email"
-                placeholder="E-mail"
-                name="email"
-                type="email"
-                containerClassName={inputClassName}
-                error={result?.errors?.email && "Este campo es obligatorio"}
-              />
-            </div>
-            <div className="flex-grow">
-              <Textarea
-                id="content"
-                data-testid="contact-textarea-content"
-                placeholder={"¿Cual es tu objetivo?"}
-                rows={1}
-                name="content"
-                error={result?.errors?.content && "Este campo es obligatorio"}
-              />
-            </div>
-          </div>
-          <div className="flex justify-center">
-            <ReCaptcha siteKey={siteKey} error={result?.errors?.reCaptcha} />
-          </div>
-          <Button
-            secondary
-            bigger
-            type="submit"
-            className="mx-auto w-fit px-5 uppercase hover-hover:hover:bg-primary hover-hover:hover:text-white"
-          >
-            <img className="mr-2" src={sendIcon} alt={"send icon"} />
-            envía tu mensaje
-          </Button>
-        </Form>
+        <ContactForm result={result} siteKey={siteKey} ref={ref} />
       </div>
     </Section>
   );
