a338e4d85d08b80795b3cef415e30a6e8e8a8723
Tosini13 <jbartos13@gmail.com>
2023-04-23 08:55:07 +0200

    remove doubled code

--- app/components/faq/Faq.tsx
+++ app/components/faq/Faq.tsx
@@ -5,7 +5,7 @@ import Answer from "./Answer";
 
 const themeClassNames = {
   question:
-    "noFocus flex cursor-pointer select-none items-center rounded-lg bg-primary-light text-primary px-4 py-4 md:py-8 transition-all duration-150",
+    "highlight-none flex cursor-pointer select-none items-center rounded-lg bg-primary-light text-primary px-4 py-4 md:py-8 transition-all duration-150",
   arrow:
     "relative ml-auto h-[2px] min-h-[2px] min-w-[10px] w-[10px] before:transition-all before:duration-500 after:transition-all after:duration-500 before:absolute before:left-0 before:h-full before:w-full before:translate-x-[35%] before:-rotate-[40deg] before:bg-primary after:absolute after:right-0 after:h-full after:w-full after:-translate-x-[35%] after:rotate-[40deg] after:bg-primary",
 };
